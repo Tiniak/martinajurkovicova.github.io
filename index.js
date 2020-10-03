2d5e16428358170019d25f6b3b0364a4028ccfef
Tiniak <72159552+Tiniak@users.noreply.github.com>
2020-10-03 16:34:27 +0200

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+let myVariable = true;
+if (myVariable === true) {
+  console.log('The condition has been evaluated to true');
+}
+let a = 7;
+if (a < 5) {
+  console.log(a / 2);
+} else {
+  console.log(a * 2);
+}
