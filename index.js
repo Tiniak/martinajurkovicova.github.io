874318ab43d5aa7e8f9cf97c67fbb2afd663b526
Tiniak <72159552+Tiniak@users.noreply.github.com>
2020-10-03 22:12:09 +0200

    exercise4

--- index.js
+++ index.js
@@ -2,9 +2,77 @@ let myVariable = true;
 if (myVariable === true) {
   console.log('The condition has been evaluated to true');
 }
-let a = 7;
-if (a < 5) {
-  console.log(a / 2);
-} else {
-  console.log(a * 2);
+//let a = 3;
+//if (a < 5) {
+ // console.log(a / 2);
+//} else {
+ // console.log(a * 2);
+//}
+
+//let bookCount = 10;
+//for (let i =1; i <= bookCount; i++){
+//   console.log (i);
+//}
+
+
+//let b;
+//for (b = 1; b <= 20; b++){
+   // if ((b % 2) == 0)
+   // console.log (b);
+//};
+
+//let add = (a, b) => {
+    //return a + b;  // the return keyword will give you back the result
+  //};
+  //console.log(add(1, 2)) // will print 3
+
+let payBill = (amount, beneficiary) => {
+    return 100 + 200;
+    console.log (payBill(amount, beneficiary));
+};
+
+//let N = 1;
+//console.log (N);
+//for (let i =1; i <= 20; i++){
+   //console.log (i);
+//}
+
+//let N = 1;
+//console.log (N);
+//for (let i = 1; i <= 15; i++){
+  //  console.log (i);
+//}
+let name= "Mark";
+function getGreetingTo(){
+    console.log("Hello " + name);
 }
+getGreetingTo();
+                                                                                                                                                            
+//let name = 'Mark';
+//let greeting = `Hello ${name}!`;
+// greeting equals to "Hello Mark!"
+
+//let N = 1;
+//function printNumbersTill(){
+  //  for (let i = 1; i <= 20; i++){
+//    console.log(i);
+//}}
+//console.log(N);
+//printNumbersTill();
+
+//let printValues = [0, 3, 6, 7, 9]
+//console.log(printValues.length);
+//console.log(printValues[0]);
+
+let array = [0, 3, 6, 7, 9]
+    for (let i = 0; i < array.length; i++) {
+      console.log(array[i]);
+  };
+
+  let myNumbers = [10, 20, 30, 50, 12];
+  myNumbers.forEach((Number))
+    for (let i = 0; i < myNumbers.length; i++) {
+        console.log(myNumbers[i]);
+    }
+  ;
+
